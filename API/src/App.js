0f9b88faa9e7e98d32ae9923fbfd1994529ddca2
jkyleBlackmon <jkyle.blackmon@gmail.com>
2021-11-07 14:16:32 -0500

    Merging dependents into api/src

--- API/src/App.js
+++ API/src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button 
+          className="CamButton"
+        >
+          Start Scanning
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
