1cabed9a9a597757212866d1e92f60a658b51250
jkyleBlackmon <jkyle.blackmon@gmail.com>
2021-11-07 15:22:07 -0500

    testing removal of node_modules in /api

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState,useEffect } from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import { BarCodeScanner, BarcodeScanner } from 'expo-barcode-scanner';
 
-function App() {
+export default function App(){
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Not yet scanned');
+
+  
+  const askCamPeremission = () =>{
+    (async()=>{
+      const {status} = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status, 'granted');
+    })
+  }
+  //Request Camera Permission
+  useEffect(()=>{
+    askCamPeremission();
+  },[]);
+
+  //After scan -- what happens?
+  const handleBarcodeScan = ({type, data}) => {
+    setScanned(true);
+    setText(data);
+    console.log('Type: ' + type + '\nData: ' + data + '\n');
+  }
+
+  //Check Permissions
+  if(hasPermission == null){
+    return(
+      <View style={styles.container}>
+        <Text>Requesting for cam permission</Text>
+      </View>
+    )
+  }
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +58,5 @@ function App() {
       </header>
     </div>
   );
-}
 
-export default App;
+};
